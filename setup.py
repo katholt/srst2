ec0b1dcd03ff1c049a5e827a92e12d637cc8655a
Ben Taylor <ben.taylor@sanger.ac.uk>
2015-12-22 16:54:50 +0000

    Add notes to README, CHANGES and bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='srst2',
-    version='0.1.6',
+    version='0.1.7',
     author='Kathryn Holt',
     author_email='drkatholt@gmail.com',
     packages=['srst2'],
